d83a153c43bc90818a45946b776e8a9a5aa2cb61
qizhixin <13284167983@163.com>
2021-03-15 20:50:31 +0800

    发布组件到npm,测试通过，可以正常使用

---
+++ index.js
@@ -0,0 +1,6 @@
+import Button from './src/Button'
+import Switch from './src/Switch'
+import Collapse from './src/Collapse'
+import CollapseItem from './src/CollapseItem'
+
+export { Button, Switch, Collapse, CollapseItem }
