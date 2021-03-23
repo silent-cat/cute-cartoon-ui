a8b969fe758de7bdedacf36b544b68171a6f00b5
qizhixin <13284167983@163.com>
2021-03-23 21:58:33 +0800

    完成分页组件

---
+++ src/Pagination.vue
@@ -0,0 +1,212 @@
+<template>
+  <ul class="cc-pagination">
+    <c-button
+      :class="['cc-pagination-item', 'cc-pagination-item--prev']"
+      :disabled="index === 1"
+      @click="prev"
+      icon="left"
+    >
+    </c-button>
+    <c-button
+      :class="['cc-pagination-item', 'cc-pagination-item--first']"
+      :disabled="index === 1"
+      @click="first"
+    >
+      首页
+    </c-button>
+    <c-button :class="['cc-pagination-item', 'cc-pagination-item--more']" v-if="showPrevMore">
+      ...
+    </c-button>
+    <c-button
+      :class="['cc-pagination-item', { 'cc-pagination-item--current': index === pager }]"
+      v-for="pager in pagers"
+      @click="go(pager)"
+    >
+      {{ pager }}
+    </c-button>
+    <c-button :class="['cc-pagination-item', 'cc-pagination-item--more']" v-if="showNextMore">
+      ...
+    </c-button>
+    <c-button
+      :class="['cc-pagination-item', 'cc-pagination-item--last']"
+      :disabled="index === pages"
+      @click="last"
+    >
+      尾页
+    </c-button>
+    <c-button
+      :class="['cc-pagination-item', 'cc-pagination-item--next']"
+      :disabled="index === pages"
+      @click="next"
+      icon="right"
+      position="right"
+    >
+    </c-button>
+  </ul>
+</template>
+<script>
+import Button from './Button'
+import ButtonGroup from './ButtonGroup'
+import Icon from './Icon'
+export default {
+  components: {
+    'c-button': Button,
+    'c-button-group': ButtonGroup,
+    'c-icon': Icon
+  },
+  //通过props来接受从父组件传递过来的值
+  props: {
+    //页面中的可见页码，其他的以...替代, 必须是奇数
+    perPages: {
+      type: Number,
+      default: 5
+    },
+
+    //当前页码
+    pageIndex: {
+      type: Number,
+      default: 1
+    },
+
+    //每页显示条数
+    pageSize: {
+      type: Number,
+      default: 10
+    },
+
+    //总记录数
+    total: {
+      type: Number,
+      default: 1
+    }
+  },
+  methods: {
+    prev() {
+      if (this.index > 1) {
+        this.go(this.index - 1)
+      }
+    },
+    next() {
+      if (this.index < this.pages) {
+        this.go(this.index + 1)
+      }
+    },
+    first() {
+      if (this.index !== 1) {
+        this.go(1)
+      }
+    },
+    last() {
+      if (this.index != this.pages) {
+        this.go(this.pages)
+      }
+    },
+    go(page) {
+      if (this.index !== page) {
+        this.index = page
+        //父组件通过change方法来接受当前的页码
+        this.$emit('change', this.index)
+      }
+    }
+  },
+  computed: {
+    //计算总页码
+    pages() {
+      return Math.ceil(this.size / this.limit)
+    },
+
+    //计算页码，当count等变化时自动计算
+    pagers() {
+      const array = []
+      const perPages = this.perPages
+      const pageCount = this.pages
+      let current = this.index
+      const _offset = (perPages - 1) / 2
+
+      const offset = {
+        start: current - _offset,
+        end: current + _offset
+      }
+
+      //-1, 3
+      if (offset.start < 1) {
+        offset.end = offset.end + (1 - offset.start)
+        offset.start = 1
+      }
+      if (offset.end > pageCount) {
+        offset.start = offset.start - (offset.end - pageCount)
+        offset.end = pageCount
+      }
+      if (offset.start < 1) offset.start = 1
+
+      this.showPrevMore = offset.start > 1
+      this.showNextMore = offset.end < pageCount
+
+      for (let i = offset.start; i <= offset.end; i++) {
+        array.push(i)
+      }
+
+      return array
+    }
+  },
+  data() {
+    return {
+      index: this.pageIndex, //当前页码
+      limit: this.pageSize, //每页显示条数
+      size: this.total || 1, //总记录数
+      showPrevMore: false,
+      showNextMore: false
+    }
+  },
+  watch: {
+    pageIndex(val) {
+      this.index = val || 1
+    },
+    pageSize(val) {
+      this.limit = val || 10
+    },
+    total(val) {
+      this.size = val || 1
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.cc-pagination {
+  > .cc-pagination-item {
+    background-color: #f4f4f5;
+    color: #000;
+    margin-left: 0.2rem;
+    &:first-child {
+      margin-left: 0;
+    }
+    &:nth-child(2) {
+      margin-left: 0;
+    }
+    &:nth-child(3) {
+      margin-left: 0;
+    }
+    &:nth-last-child(2) {
+      margin-left: 0;
+    }
+    &:last-child {
+      margin-left: 0;
+    }
+    &.cc-pagination-item--more {
+      background-color: #fff;
+      color: #505050;
+    }
+    //禁用
+    &.cc-pagination-item--more,
+    &.cc-pagination-item--current {
+      cursor: default;
+    }
+    //选中
+    &.cc-pagination-item--current {
+      background-color: #409eff;
+      color: #fff;
+    }
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import TabsBody from './TabsBody'
 import TabsHead from './TabsHead'
 import TabsItem from './TabsItem'
 import TabsPanel from './TabsPanel'
+import Pagination from './Pagination'
 
 Vue.component('c-button', Button)
 Vue.component('c-button-group', ButtonGroup)
@@ -30,6 +31,7 @@ Vue.component('c-tabs-body', TabsBody)
 Vue.component('c-tabs-head', TabsHead)
 Vue.component('c-tabs-item', TabsItem)
 Vue.component('c-tabs-panel', TabsPanel)
+Vue.component('c-pagination', Pagination)
 new Vue({
   el: '#app',
   created() {},
@@ -39,8 +41,17 @@ new Vue({
       checked1: true,
       selected: ['1'],
       value1: '',
-      selectedTab: 'tab1'
+      selectedTab: 'tab1',
+
+      pageSize: 2, //每页显示20条数据
+      currentPage: 1, //当前页码
+      count: 0, //总记录数
+      items: []
     }
+  },
+  mounted() {
+    //请求第一页数据
+    this.getList()
   },
   methods: {
     f1() {
@@ -48,6 +59,50 @@ new Vue({
     },
     fn() {
       console.log('事件被触发')
+    },
+    getList() {
+      //模拟
+      // let url = `/api/list/?pageSize=${this.pageSize}&currentPage=${this.currentPage}`
+      // this.$http.get(url).then(({ body }) => {
+      //   //子组件监听到count变化会自动更新DOM
+      //   this.count = body.count
+      //   this.items = body.list
+      // })
+
+      this.count = 25;
+      this.items = [
+        {id:1},
+        {id:2},
+        {id:3},
+        {id:4},
+        {id:5},
+        {id:6},
+        {id:7},
+        {id:8},
+        {id:9},
+        {id:10},
+        {id:11},
+        {id:12},
+        {id:13},
+        {id:14},
+        {id:15},
+        {id:16},
+        {id:17},
+        {id:18},
+        {id:19},
+        {id:20},
+        {id:21},
+        {id:22},
+        {id:23},
+        {id:24},
+        {id:25}
+      ]
+    },
+
+    // //从page组件传递过来的当前page
+    pageChange(page) {
+      this.currentPage = page
+      this.getList()
     }
   }
 })
